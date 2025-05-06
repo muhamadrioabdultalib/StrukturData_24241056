152a0a4c5b12e3b3c0a30283fb84e5dfc6c93d23
Muhamad Rio Abdul Talib <160714376+muhamadrioabdultalib@users.noreply.github.com>
2025-05-06 20:57:03 +0800

    Add files via upload

---
+++ Modul 1/Tipe_Data_Numerik/Float.py
@@ -0,0 +1,44 @@
+# Tipe Data Float
+
+# Tipe Data Float
+a = 3.141592
+b = -0.001
+c = 1.0
+d = float('inf') # memasukan infinity sebagai float
+
+# melihat isi variabel
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Cek tipe tada dari variabel
+print(type(a))  # <class 'float'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'float'>
+print(type(d))  # <class 'float'>
+
+# membandingkan nilai b3 dengan yang lain
+if (a < d):
+    print('lebih kecil')
+else:
+    print('lebih besar')
+
+    # Operasi aritmatika pada float
+print(a + c)
+print(b ** a)
+print(abs(a))
+
+4.141592
+(-3.3943828540367223e-10-1.618060304898697e-10j)
+3.141592
+
+# hasil operasi aritmatika pada float
+float = b * a
+print(float)
+print(type(float))
+
+# mengubah hasil operasi aritmatika float menjadi integer
+print(int(b * a))
+
+-0.0031415920000000003
