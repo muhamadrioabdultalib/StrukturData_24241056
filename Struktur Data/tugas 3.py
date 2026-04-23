f996eb2e152de2414a471934db102c2324e53b88
Muhamad Rio Abdul Talib <160714376+muhamadrioabdultalib@users.noreply.github.com>
2026-04-23 22:52:44 +0800

    Add files via upload

---
+++ Struktur Data/tugas 3.py
@@ -0,0 +1,12 @@
+while True:
+    angka = int(input("masukkan angka :"))
+    if angka >= 0:
+        break
+    print("harus positif")
+
+while angka < 4 :
+    angka += 2
+    print(f"angka sekarang = {angka}")
+    print("akbar ganteng")
+    
+print("program selsai")
